aa3c3df0d91a058ff40f5731aa685c5802b3e5ec
王月娇 <33644941+wangyuejiao@users.noreply.github.com>
2019-10-14 15:40:52 +0800

    Add files via upload

---
+++ build/precache-manifest.8162bf7dd7ff394d32d372be0611108c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87b4d8418b76918f45d65942ed0edfe8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e41a9829bd6dbea73f7c",
+    "url": "/static/css/main.0654219a.chunk.css"
+  },
+  {
+    "revision": "98353e1372ea528c3e73",
+    "url": "/static/js/2.433b1d84.chunk.js"
+  },
+  {
+    "revision": "e41a9829bd6dbea73f7c",
+    "url": "/static/js/main.8dfcc83d.chunk.js"
+  },
+  {
+    "revision": "628ea25a63cbc5b89a27",
+    "url": "/static/js/runtime-main.9f147cbc.js"
+  }
+]);
